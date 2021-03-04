7c4b996ec5ffb68724094dbef476efb2b81ec3a5
armin.keco@epro.at <armin.keco@epro.at>
2021-03-04 14:31:06 +0100

    commit database update settings

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');   //it is a logger of requests
 const bodyParser = require('body-parser'); //parsing the data from front to backend
+const mongoose = require('mongoose');
 
 //we need to add two middlewares
+const uri = `mongodb+srv://root:fRW5S26rofYbXl4c@amazonclone.mvn2u.mongodb.net/amazonclone?retryWrites=true&w=majority`;
+const connectionParams = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}
 
 const app = express();
+mongoose.connect(uri, connectionParams).then(() => {
+    console.log('Connected to database!');
+}).catch((err) => {
+    console.error(`Error connection to the database. \n${err}`);
+})
+   
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
